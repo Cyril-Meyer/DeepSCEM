f45f7951a5358569339e872b96be6f486659eda2
Cyril-Meyer <contact@cyrilmeyer.eu>
2024-04-08 13:21:27 +0200

    create mainwindow module which implement the ui functions

---
+++ mainwindow.py
@@ -0,0 +1,12 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+from mainwindowui import Ui_MainWindow
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
