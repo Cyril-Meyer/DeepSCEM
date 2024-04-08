febb76da9d1feabd23a1b7df19232e2f71fa24db
Cyril-Meyer <contact@cyrilmeyer.eu>
2024-04-08 13:25:14 +0200

    add usage of data module using CLI

--- run.py
+++ run.py
@@ -5,12 +5,35 @@ from PyQt5 import QtWidgets
 
 from mainwindow import MainWindow
 
+import data
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--create-sample', type=str, action='append', nargs='+',
                     help='create a sample from image and labels.\n<name> <image> [<label1> <label2> ...]')
 parser.add_argument('--create-dataset', type=str, action='append', nargs='+',
                     help='create a dataset from samples.\n <name> <sample1> [<sample2> <sample3> ...]')
 args = parser.parse_args()
+
+# create sample module
+if args.create_sample:
+    for sample in args.create_sample:
+        sample_parser = argparse.ArgumentParser()
+        sample_parser.add_argument('name', type=str)
+        sample_parser.add_argument('image', type=str)
+        sample_parser.add_argument('labels', type=str, nargs='*')
+        sample_args = sample_parser.parse_args(sample)
+
+        data.create_sample(sample_args.name, sample_args.image, sample_args.labels)
+
+# create dataset module
+if args.create_dataset:
+    for dataset in args.create_dataset:
+        dataset_parser = argparse.ArgumentParser()
+        dataset_parser.add_argument('name', type=str)
+        dataset_parser.add_argument('samples', type=str, nargs='*')
+        dataset_args = dataset_parser.parse_args(dataset)
+
+        data.create_dataset(dataset_args.name, dataset_args.samples)
 
 # no args = graphical mode
 if not len(sys.argv) > 1:
