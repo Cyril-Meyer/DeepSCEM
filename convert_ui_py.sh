cdc9de1dd5a1f3f28e594cf1c32a8aed5d2d3f6b
Cyril-Meyer <contact@cyrilmeyer.eu>
2024-04-19 23:57:18 +0200

    update convert ui script for new dialog

--- convert_ui_py.sh
+++ convert_ui_py.sh
@@ -2,4 +2,5 @@ pyuic5 mainwindow.ui -o mainwindowui.py
 pyuic5 window_newmodel.ui -o window_newmodelui.py
 pyuic5 window_train.ui -o window_trainui.py
 pyuic5 window_pred.ui -o window_predui.py
+pyuic5 window_eval.ui -o window_evalui.py
 pyrcc5 mainwindow.qrc -o mainwindow_rc.py
