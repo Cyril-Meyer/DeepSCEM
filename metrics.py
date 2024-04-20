59b03c9c207ae90c111348dbc992e1db387ff9b4
Cyril-Meyer <contact@cyrilmeyer.eu>
2024-04-20 15:03:22 +0200

    replace np.bool with bool as np.bool is deprecated

--- metrics.py
+++ metrics.py
@@ -2,16 +2,16 @@ import numpy as np
 
 
 def iou(true, pred):
-    true = true.astype(np.bool)
-    pred = pred.astype(np.bool)
+    true = true.astype(bool)
+    pred = pred.astype(bool)
     I = np.sum(true * pred)
     U = np.sum(true + pred)
     return I/U
 
 
 def f1(true, pred):
-    true = true.astype(np.bool)
-    pred = pred.astype(np.bool)
+    true = true.astype(bool)
+    pred = pred.astype(bool)
     I = np.sum(true * pred)
     U = np.sum(true + pred)
     return (2*I)/(U+I)
