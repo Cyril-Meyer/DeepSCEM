4ae6d76fc71008e0f2076410749e47f173d6c464
Cyril-Meyer <contact@cyrilmeyer.eu>
2024-04-20 20:15:28 +0200

    create new ui for evaluation results

--- convert_ui_py.bat
+++ convert_ui_py.bat
@@ -3,4 +3,5 @@ pyuic5 window_newmodel.ui -o window_newmodelui.py
 pyuic5 window_train.ui -o window_trainui.py
 pyuic5 window_pred.ui -o window_predui.py
 pyuic5 window_eval.ui -o window_evalui.py
+pyuic5 window_eval_res.ui -o window_evalresui.py
 pyrcc5 mainwindow.qrc -o mainwindow_rc.py
